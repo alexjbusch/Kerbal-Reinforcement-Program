d58eeb7150e1f126257adf2d0e5ca963a4becafa
Riya Kumari <riyakumari@wireless-10-104-176-57.umd.edu>
2023-04-12 12:15:45 -0400

    changed model a bit

--- models.py
+++ models.py
@@ -19,13 +19,27 @@ class ActorCritic(nn.Module):
         self.state_size = state_size
         self.action_size = action_size
 
-        self.affine1 = nn.Linear(self.state_size, 128)
+        # self.affine1 = nn.Linear(self.state_size, 128)
 
         # actor's layer
-        self.action_head = nn.Linear(128, self.action_size)
+        self.actor = nn.Sequential(
+               nn.Linear(self.state_size, 128),
+               nn.ReLU(),
+               nn.Linear(128, 128),
+               nn.ReLU(),
+               nn.Linear(128, self.action_size)
+               )
+        
 
         # critic's layer
-        self.value_head = nn.Linear(128, 1)
+        self.critic =  nn.Sequential(
+               nn.Linear(self.state_size, 128),
+               nn.ReLU(),
+               nn.Linear(128, 128),
+               nn.ReLU(),
+               nn.Linear(128, 1)
+               )
+
 
         # action & reward buffer
         self.saved_actions = []
@@ -37,16 +51,15 @@ class ActorCritic(nn.Module):
         """
         forward of both actor and critic
         """
-        x = F.relu(self.affine1(x))
 
         # actor: choses action to take from state s_t
         # by returning probability of each action
-        action_prob = F.softmax(self.action_head(x), dim=-1)
+        actor = F.softmax(self.actor(x), dim=-1)
 
         # critic: evaluates being in the state s_t
-        state_values = self.value_head(x)
+        critic = self.critic(x)
 
         # return values for both actor and critic as a tuple of 2 values:
         # 1. a list with the probability of each action over the action space
         # 2. the value from state s_t
-        return action_prob, state_values
+        return actor, critic
